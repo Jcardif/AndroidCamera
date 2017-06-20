552aa8aa321b9be4c3a073d6456cc619c44201e8
Josh <Josh@JOSH>
2017-06-20 09:11:52 +0300

    Add project files.

---
+++ Camera/Activity1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Graphics;
+using Android.Widget;
+
+namespace Camera
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity
+    {
+        ImageView imgview2;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.layout1);
+            imgview2 = FindViewById < ImageView>(Resource.Id.imageView2);
+            
+
+        }
+    }
+}
---
+++ Camera/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Camera
+{
+    [Activity(Label = "Camera", MainLauncher = true, Icon = "@drawable/icon")]
+    public  class MainActivity : Activity
+    {
+        ImageView imgview1;
+        Button camerabtn;
+        Button done;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            imgview1 = FindViewById<ImageView>(Resource.Id.imageView1);
+            camerabtn = FindViewById<Button>(Resource.Id.button1);
+            done = FindViewById<Button>(Resource.Id.button2);
+
+            camerabtn.Click += Camerabtn_Click;
+            done.Click += delegate { StartActivity(typeof(Activity1)); };
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)data.Extras.Get("data");
+            imgview1.SetImageBitmap(bitmap);
+
+        }
+        private void Camerabtn_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
+
